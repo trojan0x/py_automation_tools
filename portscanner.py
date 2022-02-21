f62b1cfb9935c3e33b4d2c19216905ae537047e2
Jeff Dauda <jefdauda@gmail.com>
2022-02-21 12:45:33 -0500

    Python portscanner

--- portscanner.py
+++ portscanner.py
@@ -14,5 +14,6 @@ for port in range(1, 8050):
     result = scanner(port)
     if result:
         print("port {} is open".format(port))
+        break
     else:
         print("Port {} is closed".format(port))
